fd73cfe57ffaf285c50cdaa143a2e5d3b548fb3f
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-11-27 16:45:56 +0000

    Create a fighter objects which harms player

---
+++ client/src/objects/enemies/fighter-enemy.ts
@@ -0,0 +1,108 @@
+
+
+export class FigtherEnemy extends Phaser.GameObjects.Container{
+    #weaponComponent;
+    #isInitialized;
+    #inputComponent;
+    #shipSprite
+    #shipEngineSprite
+    #verticalMovementComponent
+    #healthComponent
+    #colliderComponent;
+    #eventBusComponent
+
+    constructor(scene, x, y){
+        super(scene,x,y, []);
+        this.#isInitialized=false
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this)
+        this.body.setSize(24,24)
+        this.body.setOffset(-12,-12)
+   
+   
+
+        this.#shipSprite = scene.add.sprite(0,0,'fighter',0);
+        this.#shipEngineSprite = scene.add.sprite(0,0,'fighter_engine').setFlipY(true);
+        this.#shipEngineSprite.play('scout_engine')
+        this.add([ this.#shipEngineSprite, this.#shipSprite]);
+
+
+
+        this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
+        this.once(Phaser.GameObjects.Events.DESTROY, ()=>{
+            this.scene.events.off(Phaser.Scenes.Events.UPDATE, this.update, this);
+        }, this)
+    }
+    
+    get weaponGameObjectGroup(){
+        return this.#weaponComponent.bulletGroup
+    }
+
+    get WeaponComponent(){
+        return this.#weaponComponent;
+    }
+
+    get colliderComponent(){
+        return this.#colliderComponent;
+    }
+
+
+    get healthComponent(){
+        return this.#healthComponent;
+    }
+
+    get shipAssetKey(){
+        return 'fighter'
+    }
+
+    get shipDestroyedAnimationKey(){
+        return 'fighter_destroy'
+    }
+
+    // init(eventBusComponent){
+    //     this.#eventBusComponent = eventBusComponent
+    //     this.#inputComponent = new BotFightScoutInputComponent()
+    //     this.#verticalMovementComponent = new VerticalMovementComponent(this, this.#inputComponent, CONFIG.FIGHT_MOVEMENT_VERTICAL_VELOCITY)
+
+    //     this.#weaponComponent = new WeaponComponent(this, this.#inputComponent,{
+    //         speed: CONFIG.ENEMY_FIGHTER_BULLET_SPEED,
+    //         interval: CONFIG.ENEMY_FIGHTER_BULLET_INTERVAL,
+    //         lifespan: CONFIG.ENEMY_FIGHTER_BULLET_LIFESPAN,
+    //         maxCount: CONFIG.ENEMY_FIGHTER_BULLET_MAX_COUNT,
+    //         yOffset: 10,
+    //         flipY: false,
+    //     }, this.#eventBusComponent)
+
+    //     this.#healthComponent = new HealthComponent(CONFIG.PLAYER_HEALTH)
+    //     this.#colliderComponent = new ColliderComponent(this.#healthComponent, this.#eventBusComponent)
+    //     this.#eventBusComponent.emit(CUSTOM_EVENTS.ENEMY_INIT, this)
+    //     this.#isInitialized=true;
+    // }
+
+
+    reset(){
+        this.setActive(true)
+        this.setVisible(true) 
+        this.#healthComponent.reset()
+        this.#verticalMovementComponent.reset()
+    }
+
+    update(ts: any,dt: any){
+        if(!this.#isInitialized){
+            return
+        }
+        if(!this.active){
+            return
+        }
+        if(this.#healthComponent.isDead){
+            this.setActive(false)
+            this.setVisible(false)
+            this.#eventBusComponent.emit(CUSTOM_EVENTS.ENEMY_DESTROYED, this)
+        }
+
+        this.#inputComponent.update();
+        this.#verticalMovementComponent.update()
+        this.#weaponComponent.update(dt)
+   
+    }
+}
